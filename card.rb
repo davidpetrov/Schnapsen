3959b8037791c7ce7b023f7d7a4174e5c2150152
davidpetrov <davidpetrov7@gmail.com>
2014-02-08 14:16:26 +0200

    card class

---
+++ card.rb
@@ -0,0 +1,29 @@
+require_relative 'constants.rb'
+class Card
+  include Comparable
+  attr_accessor :suit, :value
+  
+  def initialize(suit, value)
+    @suit = suit
+    @value = value
+  end
+
+  def to_s
+    unicode_symbols = {:spade => "♠", :heart => "♥", :diamond => "♦", :club => "♣"}
+    "#{@value}#{unicode_symbols[@suit]}"
+  end
+
+  alias eql? ==
+
+  def hash
+    [@suit,  @value].hash
+  end
+
+  def <=>(other)
+    if suit == other.suit
+      Constants::VALUES[value] <=> Constants::VALUES[other.value]
+    else
+      Constants::SUITS[suit] <=> Constants::SUITS[other.suit] 
+    end
+  end
+end
