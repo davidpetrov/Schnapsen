b36e64537113b081793d8b5356d4239412768b43
davidpetrov <davidpetrov7@gmail.com>
2014-02-08 14:37:57 +0200

    requires deck class

--- main.rb
+++ main.rb
@@ -1,51 +1,6 @@
 require_relative 'constants.rb'
 require_relative 'card.rb'
-class Deck
-  include Enumerable
-
-  attr_accessor :size, :deck
-  def initialize(cards)
-    @size = cards.size
-    @deck = cards
-  end
-
-  def each(&block)
-    @deck.each(&block)
-  end
-
-  def to_s
-    @deck.sort.reduce("") { |string, card| string << "#{card} " }
-  end
-
-  def add(card)
-    @deck << card
-  end
-
-  def remove(card = 0)
-    if card.class == Fixnum
-      @deck.delete_at(card)
-    else
-      @deck.delete_at(@deck.index(card))
-    end
-  end
-
-  def shuffle
-    @deck.shuffle!
-  end
-
-  def empty?
-    @deck.empty?
-  end
-
-  def [](key)
-    if key.kind_of?(Integer)
-      @deck[key]
-    else
-      nil
-    end
-  end
-end
-
+require_relative 'deck.rb'
 class Set
   attr_accessor :state
   def initialize()
